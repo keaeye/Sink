bef1411355199572d3201418a79b67a7294e8052
keaeye <164905015+keaeye@users.noreply.github.com>
2025-01-22 09:24:10 +0800

    更新 Editor.vue

--- components/dashboard/links/Editor.vue
+++ components/dashboard/links/Editor.vue
@@ -147,7 +147,7 @@ const { previewMode } = useRuntimeConfig().public
         v-if="previewMode"
         class="text-sm text-muted-foreground"
       >
-        The preview mode link is valid for up to 24 hours.
+        预览模式下的短链有效期最长为24小时。
       </p>
       <AutoForm
         class="px-2 space-y-2 overflow-y-auto"
@@ -185,11 +185,11 @@ const { previewMode } = useRuntimeConfig().public
               variant="secondary"
               class="mt-2 sm:mt-0"
             >
-              Close
+              取消
             </Button>
           </DialogClose>
           <Button type="submit">
-            Save
+            保存
           </Button>
         </DialogFooter>
       </AutoForm>
