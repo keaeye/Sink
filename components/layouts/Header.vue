9fe0e437626ee0cdf9d6cf6f2279e216caaf2645
keaeye <164905015+keaeye@users.noreply.github.com>
2025-01-22 09:27:50 +0800

    更新 Header.vue

--- components/layouts/Header.vue
+++ components/layouts/Header.vue
@@ -63,7 +63,7 @@ const showMenu = ref(false)
                 class="w-full px-6 py-2 mr-0 text-gray-700 cursor-pointer dark:text-gray-300 md:px-3 md:mr-2 lg:mr-3 md:w-auto"
                 href="/dashboard"
                 title="Sink Dashboard"
-              >Dashboard</a>
+              >管理面板</a>
               <a
                 href="https://github.com/ccbikai/sink"
                 target="_blank"
